6ddb4dcca541c28f727b1c64298bef9053a5aefb
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 06:59:19 +0100

    commit zero

---
+++ Lumber Tycoon 2 Script pastebin roblox Lumber Tycoon 2 Gui .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+Script for Lumber Tycoon 2. it has Waypoints, Auto buy items, and Troll functions. And more!
+]]
