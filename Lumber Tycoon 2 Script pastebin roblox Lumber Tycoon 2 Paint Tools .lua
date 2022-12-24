509db617ea01acbae4c7c8fb97bba87fdf029fd4
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:32:27 +0100

    commit zero

---
+++ Lumber Tycoon 2 Script pastebin roblox Lumber Tycoon 2 Paint Tools .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/sLRwAhUE"))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							AUTO-FARM, DUPE MONEY - AXE, WOOD MODS, BASE TOOLS, AUTO-BUY, CAR TOOLS, OTHER.
+						
+]]
